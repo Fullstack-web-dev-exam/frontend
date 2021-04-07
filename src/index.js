fed64f8f056acb9a1386fe34944dd7d45dab06e0
Tom Schrier <tomschr@stud.ntnu.no>
2021-04-07 17:12:02 +0200

    .

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MenuAppBar from './components/AppBar'
 
 ReactDOM.render(
   <React.StrictMode>
+    <MenuAppBar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
