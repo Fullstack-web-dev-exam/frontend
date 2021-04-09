aed0d53ec01b47777c5a93239d0c64e0113c69e2
Glenn Hansen <hansenglenneirik@gmail.com>
2021-04-09 14:32:32 +0200

    account circle and assets folder

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,2 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+function Nav(props) {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
 
