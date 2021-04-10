fc157103d07d6778072c56cc3c269e63986e1f40
Tom Schrier <tomschr@stud.ntnu.no>
2021-04-11 00:15:43 +0200

    navbar dropdown menu + css

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import pageNotFoundIcon from '../../assets/page_not_found_black_24dp.svg';
 import { Link } from "react-router-dom";
+import './NotFound.css';
 
 class NotFound extends Component {
     constructor(props) {
@@ -11,8 +12,8 @@ class NotFound extends Component {
         return (
             <div className="container not-found-pop-up">
                 <img src={pageNotFoundIcon} alt="" />
-                <h1><span className="big-number">404</span> Page not found</h1>
-                <p>Return to the <Link to="/">Home Page</Link></p>
+                <h1><span className="big-number not-found-pop-up">404</span> Page not found</h1>
+                <p className="not-found-pop-up">Return to the <Link to="/">Home Page</Link></p>
             </div>
         );
     }
