720e8ec2c52ac83ab14aea0976220d8f724ea36a
Glenn Hansen <hansenglenneirik@gmail.com>
2021-04-13 00:33:55 +0200

    Added ;

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import UnauthorizedCard from './components/UnauthorizedCard/UnauthorizedCard';
 import PrivateRoute from './routes/PrivateRoute';
 import withUsersBackEnd from './components/hoc/MyProfileHOC';
 import MyProfile from './components/MyProfile/MyProfile';
-import ForgotPasswordEmailForm from './components/ForgotPasswordEmailForm/ForgotPasswordEmailForm'
+import ForgotPasswordEmailForm from './components/ForgotPasswordEmailForm/ForgotPasswordEmailForm';
 import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthConsumer } from './helpers/Auth';
