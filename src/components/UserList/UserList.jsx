87b4b0c8a658a28f0aa38361fe34555745abcba7
Glenn Hansen <hansenglenneirik@gmail.com>
2021-04-12 13:20:14 +0200

    Added updateUser to dashboard

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,10 +1,10 @@
 import './UserList.css';
 import UserListItem from '../UserListItem/UserListItem'
 
-function UserList({ users, handleDeleteClick }) {
+function UserList({ users, handleDeleteClick, handleEditClick }) {
     return (
         <ul>
-            {users.map((users) => (<UserListItem handleDeleteClick={handleDeleteClick} key={users.email} user={users} />))}
+            {users.map((users) => (<UserListItem handleEditClick={handleEditClick} handleDeleteClick={handleDeleteClick} key={users.email} user={users} />))}
         </ul>
     );
 }
