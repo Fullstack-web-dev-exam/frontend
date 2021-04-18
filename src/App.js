850c1488137093ffddcc04389a61fc5a8be9e21c
Tom Schrier <tomschr@stud.ntnu.no>
2021-04-18 14:27:01 +0200

    add StatusCard component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import AdminRoute from './routes/AdminRoute';
-import LogInForm from './components/Login/LogInForm';
-import NavBar from './components/NavBar/NavBar';
-import NotFound from './components/NotFound/NotFound';
-import UnauthorizedCard from './components/UnauthorizedCard/UnauthorizedCard';
-import PrivateRoute from './routes/PrivateRoute';
-import withUserBackEnd from './components/HOC/MyProfileHOC';
-import MyProfile from './components/MyProfile/MyProfile';
-import ForgotPasswordEmailForm from './components/ForgotPasswordEmailForm/ForgotPasswordEmailForm';
-import forgotBackend from './components/HOC/ForgotPassHOC';
-import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AdminRoute from './routes/AdminRoute';
+import Dashboard from './components/Dashboard/Dashboard';
+import ForgotPasswordEmailForm from './components/ForgotPasswordEmailForm/ForgotPasswordEmailForm';
+import LogInForm from './components/Login/LogInForm';
+import MyProfile from './components/MyProfile/MyProfile';
+import NavBar from './components/NavBar/NavBar';
+import PrivateRoute from './routes/PrivateRoute';
+import StatusCard from './components/StatusCard/StatusCard';
+import forgotBackend from './components/HOC/ForgotPassHOC';
+import navBarBackend from './components/HOC/NavBarHOC';
+import withUserBackEnd from './components/HOC/MyProfileHOC';
 import { AuthConsumer } from './helpers/Auth';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard';
-import navBarBackend from './components/HOC/NavBarHOC';
+import { ToastContainer, Slide } from 'react-toastify';
 
 class App extends Component {
   render() {
@@ -53,11 +52,11 @@ class App extends Component {
                   <Route exact path="/">
                     <h1>Home page</h1>
                   </Route>
-                  <Route exact path="/401">
-                    <UnauthorizedCard />
+                  <Route exact path="/403">
+                    <StatusCard statusCode={403} statusText="Forbidden" />
                   </Route>
                   <Route>
-                    <NotFound />
+                    <StatusCard statusCode={404} statusText="Page Not Found" />
                   </Route>
                 </Switch>
               </main>
